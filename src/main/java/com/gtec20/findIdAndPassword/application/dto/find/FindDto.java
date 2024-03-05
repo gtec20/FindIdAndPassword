6e7cf410caec91a28e8059e54a9894af1b1384dd
ChoiseU <csw980928@gmail.com>
2024-03-06 02:33:34 +0900

    feat : 기능구현

--- src/main/java/com/gtec20/findIdAndPassword/application/dto/find/FindDto.java
+++ src/main/java/com/gtec20/findIdAndPassword/application/dto/find/FindDto.java
@@ -1,5 +1,6 @@
 package com.gtec20.findIdAndPassword.application.dto.find;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,5 +17,24 @@ public class FindDto {
         this.id = id;
         this.name = name;
         this.pw = pw;
+    }
+
+    public String getType() {
+        if(
+                StringUtils.isEmpty(this.id)
+                && StringUtils.isNotEmpty(this.pw)
+                && StringUtils.isNotEmpty(this.name))
+        {
+            return "id";
+        }
+
+        if(
+                StringUtils.isNotEmpty(this.id)
+                && StringUtils.isEmpty(this.pw)
+                && StringUtils.isNotEmpty(this.name))
+        {
+            return "pw";
+        }
+        return null;
     }
 }
--- src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
+++ src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
 
@@ -18,8 +19,55 @@ public class FindIdAndPasswordController {
 
     @GetMapping("")
     public String isFind(@ModelAttribute("findDto") FindDto findDto) {
+        String type = findDto.getType();
+        if(StringUtils.isEmpty(type)) {
+            return "필수 값이 존재하지 않습니다.\n다시 입력해주세요.";
+        }
+        if(type.equals("id")) {
+            return getId(findDto);
+        }
 
-        return "";
+        return getPw(findDto);
+    }
+
+    public String getId(FindDto findDto) {
+        int nameIdx = -1;
+        for(int i = 0; i < nameList.size(); i++) {
+            if(nameList.get(i).equals(findDto.getName())) {
+                nameIdx = i;
+                break;
+            }
+        }
+
+        if(nameIdx == -1) {
+            return "이름이 존재하지 않습니다.";
+        }
+
+        if(!pwList.get(nameIdx).equals(findDto.getPw())) {
+            return "비밀번호가 잘 못 입력되었습니다.";
+        }
+
+        return idList.get(nameIdx);
+    }
+
+    public String getPw(FindDto findDto) {
+        int idIdx = -1;
+        for(int i = 0; i < idList.size(); i++) {
+            if(idList.get(i).equals(findDto.getId())) {
+                idIdx = i;
+                break;
+            }
+        }
+
+        if(idIdx == -1) {
+            return "아이디가 존재하지 않습니다.";
+        }
+
+        if(!nameList.get(idIdx).equals(findDto.getName())) {
+            return "이름이 잘 못 입력되었습니다.";
+        }
+
+        return pwList.get(idIdx);
     }
 
 }
