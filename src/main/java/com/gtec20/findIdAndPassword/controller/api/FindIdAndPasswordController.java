0db0d9f9ef5847a6fee65aac95f7026aef937e00
jeongha <jeonkk88@naver.com>
2024-03-06 21:09:23 +0900

    java : 이름 불일치 코드 제외 작성

--- src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
+++ src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
@@ -18,24 +18,100 @@ public class FindIdAndPasswordController {
 
     @GetMapping("")
     public String isFind(@ModelAttribute("findDto") FindDto findDto) {
-        if("id_find".equals(findDto.getType())) {
+        int idx;
+        String type = findDto.getType();
+        String id = findDto.getId();
+        String pw = findDto.getPw();
+        String name = findDto.getName();
 
+        if("id_find".equals(type)) {
+            idx = idFind(pw);
+
+            if (name.isEmpty()) {
+                return "이름이 입력되지 않았습니다.";
+            }
+
+            if (pw.isEmpty()) {
+                return "암호가 입력되지 않았습니다.";
+            }
+
+            if(idx == -1) {
+                return "암호가 일치하지 않습니다.";
+            } else {
+                idx = nameFind(name, idx);
+                return "회원님의 아이디는 " + idList.get(idx) + "입니다.";
+            }
         }
 
-        if ("pw_find".equals(findDto.getType())) {
+        if ("pw_find".equals(type)) {
+            idx = pwFind(id);
 
+            if (id.isEmpty()) {
+                return "아이디가 입력되지 않았습니다.";
+            }
+
+            if (name.isEmpty()) {
+                return "이름이 입력되지 않았습니다.";
+            }
+
+            if(idx == -1) {
+                return "아이디가 일치하지 않습니다.";
+            }else {
+                idx = nameFind(name, idx);
+                return "회원님의 암호는 " + pwList.get(idx) + "입니다.";
+            }
         }
         return "";
     }
 
-    //아이디 찾기(이름, 암호)
-    private int idFind (String name) {
-        for(String nowName : nameList) {
-            if(nameList.equals(nowName)) {
-                return 0;
+    //아이디 찾기(이름, 암호) 비밀번호 찾기(아이디, 이름)
+    private int idFind (String pw) {
+        for (int i = 0; i < pwList.size(); i++) {
+            if(pw.equals(pwList.get(i))) {
+                return i;
             }
         }
         return -1;
+    }
+
+    private int pwFind (String id) {
+        for(int i = 0; i < idList.size(); i++) {
+            if(id.equals(idList.get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int nameFind (String name, int idx) {
+        if(name.equals(nameList.get(idx))) {
+            return 0;
+        }
+        return -1;
+    }
+
+    private String NameCheck(String type, String fir, String sec) {
+        if ("id_find".equals(type) && fir.isEmpty()) {
+            return "이름이 입력되지 않았습니다.";
+        }
+
+        for (int i = 0; i < nameList.size(); i++) {
+            if ("id_find".equals(type) && !fir.equals(nameList.get(i))) {
+                return "이름이 일치하지 않습니다.";
+            }
+        }
+
+        if ("pw_find".equals(type) && sec.isEmpty()) {
+            return "비밀번호가 입력되지 않았습니다.";
+        }
+
+        for (int i = 0; i < nameList.size(); i++) {
+            if ("pw_find".equals(type) && !sec.equals(pwList.get(i))) {
+                return "이름이 입력되지 않았습니다";
+            }
+        }
+
+        return "이름이 일치하지 않습니다.";
     }
 
 }
