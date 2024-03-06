dad8c84cb85ce600961927ef907502a418d56095
Seungjoo <marenxj@gmail.com>
2024-03-06 20:48:02 +0900

    java id,pw 찾기

--- src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
+++ src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
@@ -19,7 +19,33 @@ public class FindIdAndPasswordController {
     @GetMapping("")
     public String isFind(@ModelAttribute("findDto") FindDto findDto) {
 
-        return "";
+        //아이디 찾기는 이름의 인덱스번호와 비밀번호의 인덱스번호가 같으면 그 같은 인덱스 번호의 id를 화면에 띄운다
+        //비밀번호 찾기는 아이디의 인덱스번호와 이름의 인덱스번호가 같으면 그 같은 인덱스 번호의 pw를 화면에 띄운다
+        //위에것들이 일치되지 않는다면 회원 정보를 확인할 수 없습니다.
+
+        String name = findDto.getName();
+        String id = findDto.getId();
+        String pw = findDto.getPw();
+
+        //아이디 찾기
+        if (name !=null && !name.isEmpty() && pw != null && !pw.isEmpty()){
+            int nameIdx = nameList.indexOf(name);
+            int pwIdx = pwList.indexOf(pw);
+            if(nameIdx != -1 && nameIdx == pwIdx){
+                return nameList.get(nameIdx) + "님의 아이디 : " + idList.get(nameIdx);
+            }
+        }
+
+        //비번 찾기
+        if(id != null && !id.isEmpty() && name != null && !name.isEmpty()){
+            int idIdx = idList.indexOf(id);
+            int nameIdx = nameList.indexOf(name);
+            if(idIdx != -1 && idIdx == nameIdx){
+                return idList.get(idIdx) + "님의 비밀번호 : " + pwList.get(idIdx);
+            }
+        }
+
+        return "회원 정보를 확인 할 수 없습니다.";
     }
 
 }
