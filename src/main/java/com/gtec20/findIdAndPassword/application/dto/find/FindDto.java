b1b591609fe00134a1639a737f5bcdb64768e06c
Seungjoo <marenxj@gmail.com>
2024-03-14 03:36:06 +0900

    java findId/findPw 분리

--- src/main/java/com/gtec20/findIdAndPassword/application/dto/find/FindDto.java
+++ src/main/java/com/gtec20/findIdAndPassword/application/dto/find/FindDto.java
@@ -3,6 +3,7 @@ package com.gtec20.findIdAndPassword.application.dto.find;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.thymeleaf.util.StringUtils;
 
 @Getter
 @Setter
@@ -11,10 +12,25 @@ public class FindDto {
     String id;
     String name;
     String pw;
+    String type;
 
-    public FindDto(String id, String name, String pw) {
+    public FindDto(String id, String name, String pw, String type) {
         this.id = id;
         this.name = name;
         this.pw = pw;
+        this.type = type;
     }
+
+    public boolean isIdCheck(){
+        //이름암호 아이디이름
+        //이름과 암호 둘 중 하나라도 비어있으면 찾을수없음
+        //아이디와 이름 둘 중 하나라도 비어있으면 찾을수없음
+        return StringUtils.isEmpty(name)||StringUtils.isEmpty(pw);
+    }
+
+    public boolean isPwCheck(){
+        return StringUtils.isEmpty(id)||StringUtils.isEmpty(name);
+    }
+
+
 }
--- src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
+++ src/main/java/com/gtec20/findIdAndPassword/controller/api/FindIdAndPasswordController.java
@@ -1,35 +1,35 @@
 package com.gtec20.findIdAndPassword.controller.api;
 
 import com.gtec20.findIdAndPassword.application.dto.find.FindDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/find")
+//@RequestMapping("/api/find")
 public class FindIdAndPasswordController {
 
     List<String> nameList = List.of("최승우", "채승주", "유정하");
     List<String> idList = List.of("seungwoo", "seungjoo", "jeongha");
     List<String> pwList = List.of("password1", "password2", "password3");
 
-    @GetMapping("")
-    public String isFind(@ModelAttribute("findDto") FindDto findDto) {
+    @GetMapping("/api/find")
+    public String isFindId(@ModelAttribute("findDto") FindDto findDto) {
 
         //아이디 찾기는 이름의 인덱스번호와 비밀번호의 인덱스번호가 같으면 그 같은 인덱스 번호의 id를 화면에 띄운다
         //비밀번호 찾기는 아이디의 인덱스번호와 이름의 인덱스번호가 같으면 그 같은 인덱스 번호의 pw를 화면에 띄운다
         //위에것들이 일치되지 않는다면 회원 정보를 확인할 수 없습니다.
 
         String name = findDto.getName();
-        String id = findDto.getId();
         String pw = findDto.getPw();
+        String id = findDto.getId();
 
         //아이디 찾기
         if (!StringUtils.isEmpty(name) && !StringUtils.isEmpty(pw)){
+            if(findDto.isIdCheck()){
+                return "값을 입력해주세요.";
+            }
             int nameIdx = nameList.indexOf(name);
             int pwIdx = pwList.indexOf(pw);
             if(nameIdx == -1){
@@ -38,7 +38,22 @@ public class FindIdAndPasswordController {
             if(nameIdx == pwIdx){
                 return nameList.get(nameIdx) + "님의 아이디 : " + idList.get(nameIdx);
             }
-        } else{
+        }
+        return "회원 정보를 확인 할 수 없습니다.";
+    }
+
+    @PostMapping("/api/find")
+    public String isFindPw(@ModelAttribute("findDto") FindDto findDto) {
+
+        String name = findDto.getName();
+        String id = findDto.getId();
+        String pw = findDto.getPw();
+
+        //비밀번호
+        if(!StringUtils.isEmpty(id) && !StringUtils.isEmpty(name)) {
+            if(findDto.isPwCheck()){
+                return "값을 입력해주세요.";
+            }
             int idIdx = idList.indexOf(id);
             int nameIdx = nameList.indexOf(name);
             if(idIdx == -1){
@@ -48,7 +63,6 @@ public class FindIdAndPasswordController {
                 return idList.get(idIdx) + "님의 비밀번호 : " + pwList.get(idIdx);
             }
         }
-
         return "회원 정보를 확인 할 수 없습니다.";
     }
 
